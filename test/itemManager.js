d744d53296c763153e7c7eb1baf82bbbaa1f2760
Khanh <quockhanh.abc@gmail.com>
2021-07-22 20:06:33 +0700

    a simple unit test

--- test/itemManager.js
+++ test/itemManager.js
@@ -6,12 +6,8 @@ contract('ItemManager', async (accounts) => {
         const name = 'TBP'
         const price = 123
         const res = await instance.createItem(name, price, { from: accounts[0] })
-        console.log({ res });
-        const arg = res.logs[0].args[0]
-        console.log({ arg });
-        assert.equal(res.logs[0].args[0]._itemIndex, 0, "first item should have index zero")
+        assert.equal(res.logs[0].args._index, 0, "first item should have index zero")
         const item = await instance.items(0)
-        console.log({ item })
-        assert.equal(item._indentifier, name, "indentifier should be equal to name")
+        assert.equal(item.id, name, "indentifier should be equal to name")
     })
 })
